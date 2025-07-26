92f460a22d300a0944da2b4483d760f8e4fae74e
JagadeeshRallabandi <rjagadeeshnit@gmail.com>
2025-07-26 20:42:03 +0530

    bhavya

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 4000;
 await connectDB();
 await connectCloudinary();
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173','https://jagasgreencart.vercel.app']
+const allowedOrigins = ['http://localhost:5173','https://bhavyagreencart.vercel.app']
 
 app.post('/stripe',express.raw({type:'application/json'}), stripeWebhooks);
 //Middleware configurations
